001cae8d7d0033680879547995be7f73b0ff47b5
Tetiana Klapchenko <tklapchenko@gmail.com>
2024-08-10 14:10:47 +0300

    Add folder jsBasics

---
+++ jsBasics/task2.js
@@ -0,0 +1,9 @@
+// Використовуючи конкатенацію рядків, створіть новий рядок, який містить вітання для обох осіб. 
+const guest1 = 'Alex';
+const guest2 = 'Sonya';
+const greeting1 = 'Welcome, ' + guest1 + ' and ' + guest2 + '!'; 
+console.log(greeting1);
+
+// Використайте шаблонний рядок для створення того ж вітання
+const greeting2 = `Welcome, ${guest1} and ${guest2}!`;
+console.log(greeting2);
---
+++ jsBasics/task4.js
@@ -0,0 +1,16 @@
+// Обчисліть площу кола за формулою π * radius^2
+let radius1 = 5.2;
+let square1 = (Math.PI * (Math.pow(radius1, 2))).toFixed(2);
+console.log('Площа кола:', square1);
+
+// Обчисліть площу прямокутника за формулою length * width
+let length = 3.5;
+let width = 1.8;
+let square2 = (length * width).toFixed(2);
+console.log('Площа прямокутника:', square2);
+
+// Обчисліть об'єм циліндра за формулою π * radius^2 * height
+let radius2 = 7;
+let height = 12;
+let square3 = (Math.PI * (Math.pow(radius2, 2)) * height).toFixed(2);
+console.log('Площа циліндра:', square3);
